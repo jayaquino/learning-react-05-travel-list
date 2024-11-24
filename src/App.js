2d817c4fb56769dfe88ca8d491bfabd9d0f74900
Nelson Aquino Jr. <naquinojr@outlook.com>
2024-11-24 06:50:28 -0800

    feature(state): derived state sorting

--- src/App.js
+++ src/App.js
@@ -74,10 +74,25 @@ function Form({ onAddItems }) {
 }
 
 function PackingList({ items, onDeleteItem, onToggleItem }) {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+
+  if (sortBy === "input") sortedItems = items;
+
+  if (sortBy === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+
+  if (sortBy === "packed")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             item={item}
             key={item.id}
@@ -86,6 +101,13 @@ function PackingList({ items, onDeleteItem, onToggleItem }) {
           />
         ))}
       </ul>
+      <div class="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description"> Sort by descriptions</option>
+          <option value="packed"> Sort by packed status</option>
+        </select>
+      </div>
     </div>
   );
 }
